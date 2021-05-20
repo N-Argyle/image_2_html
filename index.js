3051df619275c695b2a8319ef89a8d751237f1a4
Nicholas Argyle <nick@ip-192-168-86-141.ec2.internal>
2021-05-20 14:17:57 -0400

    Use canvas instead of all divs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sharp = require('sharp');
 
-module.exports = async (width, height, blurAmount, buffer) => {
+const main = async (width, height, blurAmount, buffer) => {
   const { data, info } = await sharp(buffer)
     .removeAlpha()
     .raw()
@@ -41,30 +41,106 @@ module.exports = async (width, height, blurAmount, buffer) => {
     [...rgbGrouped[Math.floor(numberPixels - 1)]],
   ];
 
-  const gradient = `
-    <div style="height: ${height}px; width: ${width}px; overflow: hidden;">
-      <div style="background: rgb(${gradientPoints[7].toString()}); width: 100%; height: 100%;">
-        <div style="display: flex; width: 100%; height: 100%; flex-wrap: wrap; filter: blur(${blurAmount}px);">
-          <div style="background: rgb(${gradientPoints[0].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[1].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[2].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[3].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[4].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[5].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[6].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[7].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[8].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[9].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[10].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[11].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[12].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[13].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[14].toString()}); width: 25%; height: 25%"></div>
-          <div style="background: rgb(${gradientPoints[15].toString()}); width: 25%; height: 25%"></div>
-        </div>
+  const id = 'canv_' + Math.random().toString(36).substring(7);
+
+  let nodes = '';
+
+  gradientPoints.forEach((g, i) => {
+    const suID = 'canv_pixel_' + Math.random().toString(36).substring(7);
+    let xOffset;
+    let yOffset;
+    switch (i) {
+      case 1:
+        xOffset = 0;
+        yOffset = 0;
+        break;
+      case 1:
+        xOffset = 0.25;
+        yOffset = 0;
+        break;
+      case 2:
+        xOffset = 0.5;
+        yOffset = 0;
+        break;
+      case 3:
+        xOffset = 0.75;
+        yOffset = 0;
+        break;
+      case 4:
+        xOffset = 0;
+        yOffset = 0.25;
+        break;
+      case 5:
+        xOffset = 0.25;
+        yOffset = 0.25;
+        break;
+      case 6:
+        xOffset = 0.5;
+        yOffset = 0.25;
+        break;
+      case 7:
+        xOffset = 0.75;
+        yOffset = 0.25;
+        break;
+      case 8:
+        xOffset = 0;
+        yOffset = 0.5;
+        break;
+      case 9:
+        xOffset = 0.25;
+        yOffset = 0.5;
+        break;
+      case 10:
+        xOffset = 0.5;
+        yOffset = 0.5;
+        break;
+      case 11:
+        xOffset = 0.75;
+        yOffset = 0.5;
+        break;
+      case 12:
+        xOffset = 0;
+        yOffset = 0.75;
+        break;
+      case 13:
+        xOffset = 0.25;
+        yOffset = 0.75;
+        break;
+      case 14:
+        xOffset = 0.5;
+        yOffset = 0.75;
+        break;
+      case 15:
+        xOffset = 0.75;
+        yOffset = 0.75;
+        break;
+      default:
+        xOffset = 0;
+        yOffset = 0;
+        break;
+    }
+    nodes += `
+      const ${suID} = c.getContext("2d");
+      ${suID}.beginPath();
+      ${suID}.rect(${xOffset} * c.width, ${yOffset} * c.height, .25 * c.width, .25 * c.height);
+      ${suID}.fillStyle = 'rgb(${gradientPoints[i].toString()})';
+      ${suID}.fill();
+    `;
+  });
+
+  const canvas = `
+    <div style="height: 350px; width: 500px; background: rgb(${gradientPoints[7].toString()}); overflow: hidden;">
+      <div style=" width: 100%; height: 100%;filter: blur(${blurAmount}px);">
+        <canvas id="${id}" style="height: ${height}px; width: ${width}px;"></canvas>
+        <script>
+          const c = document.getElementById("${id}");
+          ${nodes}
+        </script>
       </div>
     </div>
   `;
 
-  return gradient;
+  return canvas;
 };
+
+module.exports = main();
